1173af63dae4712f17106e49da6bd642f2d71d63
bagus tri harjanto <bagustri15@gmail.com>
2021-04-30 13:50:22 +0700

    Add feature send email

--- src/modules/auth/auth_controller.js
+++ src/modules/auth/auth_controller.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const helper = require('../../helpers/wrapper')
 const authModel = require('./auth_model')
+const nodemailer = require('nodemailer')
+require('dotenv').config()
 
 module.exports = {
   register: async (req, res) => {
@@ -20,9 +22,39 @@ module.exports = {
       // kondisi cek email apakah ada di dalam database ?
       // jika ada response gagal msg = email sudah pernah di daftarkan
       // jika tidak ada = menjalankan proses model register user
-      const result = await authModel.register(setData)
-      delete result.user_password
-      return helper.response(res, 200, 'Success Register User', result)
+      // https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4N78ToeMZfGrjVbk-kR95NcXdeZBPjuMIbllyX9vO7Te7z9HXQNo8LA0WUR_ACHHXAg2l4G67dmKBk8loxwQiA_wxRYvQ
+      console.log(process.env.SMTP_EMAIL)
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: process.env.SMTP_EMAIL, // generated ethereal user
+          pass: process.env.SMTP_PASSWORD // generated ethereal password
+        }
+      })
+
+      const mailOptions = {
+        from: '"Tickitz 👻" <memo.in.aja@gmail.com>', // sender address
+        to: userEmail, // list of receivers
+        subject: 'Tickitz - Activation Email', // Subject line
+        html:
+          "<b>Click Here to activate</b><a href='http://localhost:3001/api/v1/user-activation/1'>Click !</>" // html body
+      }
+
+      await transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error)
+          return helper.response(res, 400, 'Email not send !')
+        } else {
+          console.log('Email sent:' + info.response)
+          return helper.response(res, 200, 'Success Register User')
+        }
+      })
+
+      // const result = await authModel.register(setData)
+      // delete result.user_password
+      // return helper.response(res, 200, 'Success Register User', result)
     } catch (error) {
       return helper.response(res, 400, 'Bad Request', error)
     }
